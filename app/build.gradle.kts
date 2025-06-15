01a981b2e4c6c40c203eca4e81275a7afa16fcf9
Chathumi Rathnayaka <chathumi.rathnayaka25@gmail.com>
2025-06-15 10:51:38 +0530

    commit 27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-firestore:24.10.3")
     implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("com.google.android.material:material:1.12.0")
+
+
 
 
     implementation(libs.firebase.storage)
